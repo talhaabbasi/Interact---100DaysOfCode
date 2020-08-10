b100465784eb1ab97e0588f142a4411593f60bdb
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-08-10 11:44:18 +0500

    Topic Model created

---
+++ WebAPI/Models/Topic.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Topic
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string TopicId { get; set; }
+
+        [BsonElement("Name")]
+        public string TopicName { get; set; }
+
+        public int Viewers { get; set; }
+    }
+}
