5302ead6e8822ed1a9c918af6a68609dea32b148
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-08-12 10:37:25 +0500

    Controller modified for methods

---
+++ WebAPI/Controllers/TopicsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicsController : ControllerBase
+    {
+        private readonly TopicService _topicService;
+
+        public TopicsController(TopicService topicService)
+        {
+            _topicService = topicService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Topic>> Get() =>
+            _topicService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetTopic")]
+        public ActionResult<Topic> Get(string id)
+        {
+            var topic = _topicService.Get(id);
+
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return topic;
+        }
+
+        [HttpPost]
+        public ActionResult<Topic> Create(Topic topic)
+        {
+            _topicService.Create(topic);
+
+            return CreatedAtRoute("GetTopic", new { id = topic.TopicId.ToString() }, topic);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Topic topicIn)
+        {
+            var topic = _topicService.Get(id);
+
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            _topicService.Update(id, topicIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var topic = _topicService.Get(id);
+
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            _topicService.Remove(topic.TopicId);
+
+            return NoContent();
+        }
+    }
+}
